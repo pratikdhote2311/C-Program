7b43785b570dc485e18f7685ee01ca87d3a02072
pratikdhote2311 <98937820+pratikdhote2311@users.noreply.github.com>
2022-02-04 10:38:43 +0530

    Add files via upload

---
+++ a+b,a-b.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int a,b,total;
+	cout<<"Enter the values of a and b\n";
+	cin>>a>>b;
+	
+	total=a*a+2*a*b+b*b;
+	cout<<total;
+	
+	return 0;
+}
